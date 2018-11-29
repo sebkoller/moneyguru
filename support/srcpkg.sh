59802d70e385ef51062f18aa63af4113b7720e1a
Virgil Dupras <hsoft@hardcoded.net>
2018-11-29 12:09:04 -0500

    Adjust "make srcpkg"
    We don't have submodules any more.

--- support/srcpkg.sh
+++ support/srcpkg.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
 echo "Creating git archive"
-version=`python3 -c "from core.app import Application; print(Application.VERSION)"`
+version=`python3 -c "from core import __version__; print(__version__)"`
 dest="moneyguru-src-${version}.tar"
 
 git archive -o ${dest} HEAD
-
-# Now, we need to include submodules
-submodules="hscommon qtlib"
-
-for submodule in $submodules; do
-    echo "Adding submodule ${submodule} to archive"
-    archive_name="${submodule}.tar"
-    git -C ${submodule} archive -o ../${archive_name} --prefix ${submodule}/ HEAD
-    tar -A ${archive_name} -f ${dest}
-    rm ${archive_name}
-done
-
 gzip -f ${dest}
 echo "Built source package ${dest}.gz"
