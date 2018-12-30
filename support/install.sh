2efc9acd9585b7e916a832b7d986e2376d3a35f3
Virgil Dupras <hsoft@hardcoded.net>
2018-12-30 18:55:41 -0500

    Makefile: don't copy tests during install

--- support/install.sh
+++ support/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ "$#" -ne 4 ]]; then
     echo "Wrong number of arguments"
     exit 1
@@ -13,7 +15,11 @@ PIXMAP="${PREFIX}/share/pixmaps/moneyguru.png"
 install -D run.py "${D}${DESTLIB}/run.py"
 mkdir -p "${D}${PREFIX}/bin"
 ln -sf "${DESTLIB}/run.py" "${D}${PREFIX}/bin/moneyguru"
-cp -rf core qt "${D}${DESTLIB}"
+cp -rf qt "${D}${DESTLIB}"
+# copy all core except core/tests
+mkdir -p "${D}${DESTLIB}/core"
+cp -f core/*.py "${D}${DESTLIB}/core"
+cp -rf core/{model,gui,plugin,saver,loader,__pycache__} "${D}${DESTLIB}/core"
 mkdir -p "${D}${PREFIX}/share/applications"
 install -D -m644 support/moneyguru.desktop \
     "${D}${PREFIX}/share/applications/moneyguru.desktop"
