bf6b8882577b3141732648ddaa4aba76e18f0f6a
Virgil Dupras <hsoft@hardcoded.net>
2019-01-04 09:00:44 -0500

    Make Broadcaster notify its listeners in a predictable order
    Having `listeners` as a set is a big problem for my "de-notification"
    refactoring because I can't "flatten" the notification propagation (for
    example, by making `MainWindow` a simple `Listener` and make its
    children listen to `Document` directly) without wreaking havoc: children
    start reacting to notifications before the main window and the code
    isn't written for that).
    
    So, to continue that flattening, I'm making `listeners` into a list.

--- core/notify.py
+++ core/notify.py
@@ -1,7 +1,7 @@
-# Copyright 2015 Hardcoded Software (http://www.hardcoded.net)
+# Copyright 2019 Virgil Dupras
 
-# This software is licensed under the "GPLv3" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+# This software is licensed under the "GPLv3" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.gnu.org/licenses/gpl-3.0.html
 
 """Very simple inter-object notification system.
@@ -18,23 +18,24 @@ class Broadcaster:
     """Broadcasts messages that are received by all listeners.
     """
     def __init__(self):
-        self.listeners = set()
-    
+        self.listeners = []
+
     def add_listener(self, listener):
-        self.listeners.add(listener)
-    
+        self.listeners.append(listener)
+
     def notify(self, msg):
         """Notify all connected listeners of ``msg``.
-        
+
         That means that each listeners will have their method with the same name as ``msg`` called.
         """
-        for listener in self.listeners.copy(): # listeners can change during iteration
+        for listener in self.listeners[:]: # listeners can change during iteration
             if listener in self.listeners: # disconnected during notification
                 listener.dispatch(msg)
-    
+
     def remove_listener(self, listener):
-        self.listeners.discard(listener)
-    
+        if listener in self.listeners:
+            self.listeners.remove(listener)
+
 
 class Listener:
     """A listener is initialized with the broadcaster it's going to listen to. Initially, it is not connected.
@@ -42,27 +43,27 @@ class Listener:
     def __init__(self, broadcaster):
         self.broadcaster = broadcaster
         self._bound_notifications = defaultdict(list)
-    
+
     def bind_messages(self, messages, func):
         """Binds multiple message to the same function.
-        
+
         Often, we perform the same thing on multiple messages. Instead of having the same function
         repeated again and agin in our class, we can use this method to bind multiple messages to
         the same function.
         """
         for message in messages:
             self._bound_notifications[message].append(func)
-    
+
     def connect(self):
         """Connects the listener to its broadcaster.
         """
         self.broadcaster.add_listener(self)
-    
+
     def disconnect(self):
         """Disconnects the listener from its broadcaster.
         """
         self.broadcaster.remove_listener(self)
-    
+
     def dispatch(self, msg):
         if msg in self._bound_notifications:
             for func in self._bound_notifications[msg]:
@@ -70,20 +71,20 @@ class Listener:
         if hasattr(self, msg):
             method = getattr(self, msg)
             method()
-    
+
 
 class Repeater(Broadcaster, Listener):
     REPEATED_NOTIFICATIONS = None
-    
+
     def __init__(self, broadcaster):
         Broadcaster.__init__(self)
         Listener.__init__(self, broadcaster)
-    
+
     def _repeat_message(self, msg):
         if not self.REPEATED_NOTIFICATIONS or msg in self.REPEATED_NOTIFICATIONS:
             self.notify(msg)
-    
+
     def dispatch(self, msg):
         Listener.dispatch(self, msg)
         self._repeat_message(msg)
-    
+
