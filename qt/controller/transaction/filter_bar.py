25a635f2230539edd8781954f99309457fb1f4e4
Virgil Dupras <hsoft@hardcoded.net>
2019-01-04 17:47:49 -0500

    Fix broken qt layer

--- qt/controller/transaction/filter_bar.py
+++ qt/controller/transaction/filter_bar.py
@@ -1,13 +1,11 @@
-# Created By: Virgil Dupras
-# Created On: 2009-11-27
-# Copyright 2015 Hardcoded Software (http://www.hardcoded.net)
-# 
-# This software is licensed under the "GPLv3" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+# Copyright 2019 Virgil Dupras
+#
+# This software is licensed under the "GPLv3" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.gnu.org/licenses/gpl-3.0.html
 
 from core.trans import trget
-from core.document import FilterType
+from core.const import FilterType
 
 from ..filter_bar import FilterBar
 
